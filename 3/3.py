cdf1fdace7b1143b5a2273316f93ea3d71e5de3f
Joshua Wong <joshwonghc@gmail.com>
2019-11-15 20:59:21 +0800

    more lolz

--- 3/3.py
+++ 3/3.py
@@ -1,6 +1,6 @@
 c={}
 while 1:
-    x=input();a,b=sorted(x.split("="));
-    if a=="?":print(x.replace("?",c[b]))
-    else:c[b]=a
+  x=input();a,b=sorted(x.split("="));
+  if a=="?":print(x.replace("?",c[b]))
+  else:c[b]=a
 
