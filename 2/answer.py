bb9d5a4b31e946f63dcc177966ec840c97d50a57
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2019-11-15 20:20:21 +0800

    python 2

--- 2/answer.py
+++ 2/answer.py
@@ -1,1 +1,1 @@
-while 1:from decimal import *;d=Decimal;a,b=input().split();print("{:.99f}".format(d(a)/d(10**int(b))).rstrip('0').rstrip('.'))
+while 1:from decimal import *;d=Decimal;a,b=input().split();print(("%.99f"%(d(a)/d(10**int(b)))).rstrip('0').rstrip('.'))
