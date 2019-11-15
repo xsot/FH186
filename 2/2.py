8f001b2280c6dff4cb0fb8a382768c67e692f7da
Weehan <deantiu56@gmail.com>
2019-11-15 20:27:21 +0800

    oof

--- 2/2.py
+++ 2/2.py
@@ -1,2 +1,2 @@
 while 1:
-  a,b=map(int,input().split());print(str(('%f' % (a/10**b))).rstrip('0').rstrip('.'))
+  a,b=map(int,input().split());print(str('{:.100f}'.format(a/10**b)).rstrip('0').rstrip('.'))
