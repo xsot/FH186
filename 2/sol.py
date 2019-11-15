57449a811df696d66f333c03bbc0dd107c40bf44
Raynold Ng <raynold.ng24@gmail.com>
2019-11-15 20:19:43 +0800

    dfd

--- 2/sol.py
+++ 2/sol.py
@@ -1,1 +1,4 @@
-while 1:print('{0:g}'.format(eval(input().replace(' ','/10**'))))
+while 1:    
+    a,b=input().split()
+    if int(b) < len(a): print('{0:g}'.format(int(a)/10**int(b)))
+    else: print('0.'+('0'*(len(a)-int(b))+a.rstrip('0')))
